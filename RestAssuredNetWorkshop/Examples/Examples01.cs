bc38d9e4566a19e95612d5ad09649f68e0770dab
Bas Dijkstra <bas@ontestautomation.com>
2024-01-18 16:56:50 +0100

    Add first series of examples

---
+++ RestAssuredNetWorkshop/Examples/Examples01.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using RestAssured.Request.Logging;
+using RestAssured.Response.Logging;
+using static RestAssured.Dsl;
+
+namespace RestAssuredNetWorkshop.Examples
+{
+    public class Examples01
+    {
+        [Test]
+        public void GetUserData_VerifyName_ShouldBeLeanneGraham()
+        {
+            Given()
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/users/1")
+                .Then()
+                .AssertThat()
+                .Body("$.name", NHamcrest.Is.EqualTo("Leanne Graham"));
+        }
+
+        [Test]
+        public void LogAllRequestData()
+        {
+            Given()
+                .Log(RequestLogLevel.All)
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/users/1")
+                .Then()
+                .AssertThat()
+                .Body("$.name", NHamcrest.Is.EqualTo("Leanne Graham"));
+        }
+
+        [Test]
+        public void LogAllResponseData()
+        {
+            Given()
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/users/1")
+                .Then()
+                .Log(ResponseLogLevel.All)
+                .Body("$.name", NHamcrest.Is.EqualTo("Leanne Graham"));
+        }
+
+        [Test]
+        public void GetUserData_VerifyStatusCodeAndContentType()
+        {
+            Given()
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/users/1")
+                .Then()
+                .AssertThat()
+                .StatusCode(200)
+                .ContentType("application/json; charset=utf-8");
+        }
+    }
+}
