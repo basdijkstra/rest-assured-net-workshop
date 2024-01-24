d8b4850d78604a7b82f324f087d78638d055ab2e
basdijkstra <bas@ontestautomation.com>
2024-01-24 09:06:53 +0100

    Add fourth series of examples

---
+++ RestAssuredNetWorkshop/Examples/Examples04.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using RestAssuredNetWorkshop.Examples.Models;
+using static RestAssured.Dsl;
+
+namespace RestAssuredNetWorkshop.Examples
+{
+    public class Examples04
+    {
+        [Test]
+        public void SerializePostObjectToJson()
+        {
+            Post post = new Post
+            {
+                UserId = 1,
+                Title = "My new blog post",
+                Body = "This is an awesome piece of content"
+            };
+
+            Given()
+                .Body(post)
+                .When()
+                .Post("https://jsonplaceholder.typicode.com/posts")
+                .Then()
+                .StatusCode(201);
+        }
+
+        [Test]
+        public void SerializeAnonymousObjectToJson()
+        {
+            var post = new
+            {
+                userId = 1,
+                title = "My new blog post",
+                body = "This is an awesome piece of content"
+            };
+
+            Given()
+                .Body(post)
+                .When()
+                .Post("https://jsonplaceholder.typicode.com/posts")
+                .Then()
+                .StatusCode(201);
+        }
+
+        [Test]
+        public void DeserializeJsonToPost()
+        {
+            Post post = (Post)Given()
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/posts/1")
+                .DeserializeTo(typeof(Post));
+
+            Assert.That(post.Title, Contains.Substring("sunt aut facere"));
+        }
+
+        [Test]
+        public void DeserializeJsonToPostAfterVerification()
+        {
+            Post post = (Post)Given()
+                .When()
+                .Get("https://jsonplaceholder.typicode.com/posts/1")
+                .Then()
+                .StatusCode(200)
+                .DeserializeTo(typeof(Post));
+
+            Assert.That(post.Title, Contains.Substring("sunt aut facere"));
+        }
+    }
+}
---
+++ RestAssuredNetWorkshop/Examples/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RestAssuredNetWorkshop.Examples.Models
+{
+    internal class Post
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonProperty("body")]
+        public string Body { get; set; } = string.Empty;
+    }
+}
